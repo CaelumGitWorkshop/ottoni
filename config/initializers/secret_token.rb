83df25db90dfedd693b81422f94f7d75f13de9c3
Caelum Infra <infracaelumrj@gmail.com>
2011-10-18 20:21:27 -0200

    app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CadastroDePessoas::Application.config.secret_token = 'f62bbe8df96c21f6bed1a922082d4299dcb87f94c7bb637e1ca9b3ce4e3cc32c2bb07dcfb83fd196597c6c59cda32f97a95f6dd271e1a458d01c1af789ffeabe'
